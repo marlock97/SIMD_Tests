cc74926f66da8c9401668fabca967eb289517c7a
Martin Angulo <marlock97@gmail.com>
2020-10-26 12:52:17 +0100

    Changed profiling to micro instead of milli. Moved iterations variable to main instead of global. Testing with smaller vectors for cache reasons.

--- main.cpp
+++ main.cpp
@@ -10,9 +10,7 @@
 
 static const bool printVectors = false;
 static const bool printIterInfo = false;
-
-static const unsigned int iterations = 1000000;
-static const unsigned int vecSize = 4 * 10000;
+static const unsigned int vecSize = 192;
 
 void printNFloatVector(const char* name, unsigned int n, float* v) {
   if (!printVectors)
@@ -21,7 +19,7 @@ void printNFloatVector(const char* name, unsigned int n, float* v) {
   std::cout << name << ": ";
 
   for (unsigned int i = 0; i < n; ++i) {
-    std::cout << *(v + i);
+    std::cout << v[i];
 
     if(i < (n -1))
       std::cout << ", ";
@@ -30,7 +28,7 @@ void printNFloatVector(const char* name, unsigned int n, float* v) {
   }
 }
 
-std::chrono::duration<double, std::milli> operate(float* res, float* v1, float* v2) {
+std::chrono::duration<double, std::micro> operate(float* res, float* v1, float* v2) {
   auto start = std::chrono::high_resolution_clock::now();
   for (unsigned int i = 0; i < vecSize; ++i) {
     res[i] = v1[i] * v2[i];
@@ -39,13 +37,14 @@ std::chrono::duration<double, std::milli> operate(float* res, float* v1, float* 
   return end - start;
 }
 
-std::chrono::duration<double, std::milli> operate_simd(float* res, float* v1, float* v2) {
+std::chrono::duration<double, std::micro> operate_simd(float* res, float* v1, float* v2) {
   auto start = std::chrono::high_resolution_clock::now();
   for (unsigned int i = 0; i < vecSize; i += 4) {
     __m128 v1_128 = _mm_set_ps(v1[i + 3], v1[i + 2], v1[i + 1], v1[i]); //Load 4 bytes to v1_128 (Loads in reverse order so we input in erverse too)
     __m128 v2_128 = _mm_set_ps(v2[i + 3], v2[i + 2], v2[i + 1], v2[i]); //Load 4 bytes to v2_128 (Loads in reverse order so we input in erverse too)
     __m128 res_128 = _mm_mul_ps(v1_128, v2_128); //Add v1_128 & v2_128 and save in res_128
-    memcpy((res + i), &res_128, sizeof(res_128)); //Save result from res_128 to res
+    //memcpy((res + i), &res_128, sizeof(res_128)); //Save result from res_128 to res
+    _mm_store_ps(res + i, res_128);
   }
   auto end = std::chrono::high_resolution_clock::now();
   return end - start;
@@ -53,15 +52,17 @@ std::chrono::duration<double, std::milli> operate_simd(float* res, float* v1, fl
 
 int main()
 {
+  static const unsigned int iterations = 1000000;
+
   float v1[vecSize];
   float v2[vecSize];
   float res[vecSize];
   float res_simd[vecSize];
 
-  assert((sizeof(v1) / sizeof(float)) % 4 == 0);
-  assert((sizeof(v2) / sizeof(float)) % 4 == 0);
-  assert((sizeof(res) / sizeof(float)) % 4 == 0);
-  assert((sizeof(res_simd) / sizeof(float)) % 4 == 0);
+  static_assert((sizeof(v1) / sizeof(float)) % 4 == 0);
+  static_assert((sizeof(v2) / sizeof(float)) % 4 == 0);
+  static_assert((sizeof(res) / sizeof(float)) % 4 == 0);
+  static_assert((sizeof(res_simd) / sizeof(float)) % 4 == 0);
 
   for (unsigned int i = 0; i < vecSize; ++i) {
     v1[i] = static_cast<float>(i);
@@ -76,18 +77,19 @@ int main()
   std::vector<double> diffs_vector;
 
   for (int i = 0; i < iterations; ++i) {
-    std::chrono::duration<double, std::milli> elapsed_normal;
-    std::chrono::duration<double, std::milli> elapsed_simd;
-
+    memset(res, 0, sizeof(res));
+    memset(res_simd, 0, sizeof(res_simd));
+    std::chrono::duration<double, std::micro> elapsed_normal;
+    std::chrono::duration<double, std::micro> elapsed_simd;
     auto future = std::async(operate, res, v1, v2);
     auto future_simd = std::async(operate_simd, res, v1, v2);
     elapsed_normal = future.get();
     elapsed_simd = future_simd.get();
     /*
-    std::packaged_task<std::chrono::duration<double, std::milli>(float*, float*, float*)> job1(operate);
-    std::packaged_task<std::chrono::duration<double, std::milli>(float*, float*, float*)> job2(operate_simd);
-    std::future<std::chrono::duration<double, std::milli>> future = job1.get_future();
-    std::future<std::chrono::duration<double, std::milli>> future_simd = job2.get_future();
+    std::packaged_task<std::chrono::duration<double, std::micro>(float*, float*, float*)> job1(operate);
+    std::packaged_task<std::chrono::duration<double, std::micro>(float*, float*, float*)> job2(operate_simd);
+    std::future<std::chrono::duration<double, std::micro>> future = job1.get_future();
+    std::future<std::chrono::duration<double, std::micro>> future_simd = job2.get_future();
     std::thread worker1(std::move(job1));
     std::thread worker2(std::move(job2));
     worker1.join();
